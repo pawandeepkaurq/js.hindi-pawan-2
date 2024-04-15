33d9bfad39ada4538c238532c3afa6108df08aec
pawandeepkaurq <143918190+pawandeepkaurq@users.noreply.github.com>
2024-04-15 17:08:53 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accaountId = 144553
+let accountEmail = "pawwannk@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+//accaountId = 234 is not allowed cause its a constent
+
+accountEmail = "pawwannk2@gmail.com"
+accountPassword = "123"
+accountCity = "3"
+
+console.log(accaountId)
+console.table([accountEmail,accountPassword,accountCity])
---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,33 @@
+"use strict"; // it meanns treat all js code by newer version
+
+// alert(3+3) // in alert this is not allowed as we are using nodejs not brpwser
+
+// if i write somthing in "" it become a string
+let name = "pawan"
+
+//its a number
+let age = 18 
+
+//its a bollean
+let isLoggedIn = false
+
+//when variable is define and value is not define then it shows undefinr
+let state;
+
+//tupes of dataType
+//number => 2 to power 53
+//bigint ( we use it if number is very very bid)
+//string => "" or ''
+//boolean => true/false
+//null => standalone value
+//undefined
+//symbole => unique
+
+//non premitine datatype
+//object
+
+//to find type of data
+console.log(typeof "pawan")//string
+console.log(typeof age)//number
+console.log(typeof undefine)//undefine
+console.log(typeof null)//object

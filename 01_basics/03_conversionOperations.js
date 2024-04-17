f9baedd101ce3ba936323c552d8b206d4a31509d
pawandeepkaurq <143918190+pawandeepkaurq@users.noreply.github.com>
2024-04-17 18:36:05 +0000

    conversion of datatupes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,26 @@
+let score = "33"
+
+ console.log (typeof score);
+ console.log(typeof score)
+
+ let valueInNumber = Number(score)
+ console.log(typeof valueInNumber);
+
+ //"33" => 33 (this 33 can be conerted into number)
+ //"33abc" => NaN (this cant be converted into number cause its originally not a number)
+ //true => 1, false => 0
+  
+ let isLoggedIn = 1
+
+ let booleanIsLoggedIn = Boolean(isLoggedIn)
+ console.log( booleanIsLoggedIn)
+ //always write conversion datatype starts with capital
+ // 1 => true
+ //0 => false
+ //"" => false
+ //"pawan" => true
+  
+ let someNumber = 33
+ let stringNumber = String(someNumber)
+ console.log(stringNumber)
+ console.log(typeof stringNumber)

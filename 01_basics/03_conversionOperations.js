41f28dbcf463e0b604500d6afadb6e5fc8358d0a
pawandeepkaurq <143918190+pawandeepkaurq@users.noreply.github.com>
2024-04-17 19:08:11 +0000

    operatins

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -24,3 +24,31 @@ let score = "33"
  let stringNumber = String(someNumber)
  console.log(stringNumber)
  console.log(typeof stringNumber)
+//**********operatins ************
+// conversinn into a negative value (negValue)
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+//methamatical function
+//console.log(2+2)
+//console.log(2-2)
+//console.log(2**2)
+//console.log(2%3)
+
+//addind 2 stings
+let str1 = "hello"
+let str2 = "pawan"
+let str3 = str1+str2
+console.log(str3)
+
+//shouls not do these things
+console.log("1" + 2);
+console.log(1 +"2");
+console.log("1" +2+2);
+console.log(1 + 2 + "2");
+
+
+ 
+
+

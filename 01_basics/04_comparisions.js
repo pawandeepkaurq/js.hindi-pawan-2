6a55554c2960d155595c608604ecdaa3007fb345
pawandeepkaurq <143918190+pawandeepkaurq@users.noreply.github.com>
2024-04-17 19:22:58 +0000

    compaeision

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,21 @@
+//for comparisions
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2==1);
+console.log(2!=1);
+
+//always compare same data type ...u can compare then but its baseless
+console.log("2" > 1);
+console.log("02">1);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined>0);
+console.log(undefined<0);
+console.log(undefined==0);
+
+//=== or strict check(it compare the values as well as datatypes)
+console.log("2"===2);
